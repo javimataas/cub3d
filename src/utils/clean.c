aa5d91513dbdda84cfc41627646f92b6f136d4eb
jariza-o <juanarizaordonez@gmail.com>
2023-12-03 18:29:57 +0100

    Clear I think that is solved, but I need revise in MAC Desktop

--- src/utils/clean.c
+++ src/utils/clean.c
@@ -6,32 +6,57 @@
 /*   By: jariza-o <jariza-o@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 15:37:57 by jariza-o          #+#    #+#             */
-/*   Updated: 2023/12/02 18:50:21 by jariza-o         ###   ########.fr       */
+/*   Updated: 2023/12/03 18:28:09 by jariza-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
+void ft_free_t_texture(t_textures *texts)
+{
+	t_textures	*aux;
+
+	while (texts)
+	{
+		aux = texts->next;
+		if (texts->id != NULL)
+		{
+			free (texts->id);
+			texts->id = NULL;
+		}
+		if (texts->path != NULL);
+		{
+			free (texts->path);
+			texts->path = NULL;
+		}
+		if (texts != NULL)
+			free (texts);
+		texts = aux;
+	}
+	texts = NULL;
+}
+void	ft_free_t_map(t_map *map)
+{
+	ft_free_t_texture(map->texts);
+	if (map->map != NULL)
+	{
+		ft_free_str_array(map->map);
+		map->map = NULL;
+	}
+	if (map != NULL)
+		free (map);
+	map = NULL;
+}
+
 void	ft_clear(t_game *game)
 {
-	t_textures	*aux ;
-
-	aux = game->map->texts;
-	while (aux)
-	{
-		if (aux->id != NULL)
-			free (aux->id);
-		if (aux->path != NULL)
-			free (aux->path);
-		aux = aux->next;
-	}
-	free (game->map->texts);
-	if (game->map->map != NULL)
-		ft_free_str_array(game->map->map);
-	free (game->map);
+	ft_free_t_map(game->map);
 	if (game->file != NULL)
+	{
 		ft_free_str_array(game->file);
+		game->file = NULL;
+	}
 	if (game != NULL)
 		free (game);
-	mlx_terminate(game->mlx);
+	game = NULL;
 }
